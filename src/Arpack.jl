b19690dba5de94fc6e632bb1977ad2c7f8b52b9d
David Hong <dahong@umich.edu>
2018-10-27 10:01:23 -0400

    Use rmul! to avoid extra allocation.

--- src/Arpack.jl
+++ src/Arpack.jl
@@ -313,7 +313,7 @@ svds(A; kwargs...) = _svds(A; kwargs...)
 function _orth!(P)
     Q,R = qr!(P)
     rsign = [iszero(rval) ? one(rval) : sign(rval) for rval in diag(R)]
-    return Matrix(Q) * Diagonal(rsign)
+    return rmul!(Matrix(Q), Diagonal(rsign))
 end
 function _svds(X; nsv::Int = 6, ritzvec::Bool = true, tol::Float64 = 0.0, maxiter::Int = 1000, ncv::Int = 2*nsv, v0::Vector=zeros(eltype(X),(0,)))
     if nsv < 1
